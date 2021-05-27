0e473614d7c91845d7388c39c750885c9fe01243
Raphaël Champeimont <raphael.champeimont@almacha.org>
2021-05-27 16:50:33 +0200

    Adapt code to run on Uno

--- floppy_reader/floppy_reader.ino
+++ floppy_reader/floppy_reader.ino
@@ -1,6 +1,6 @@
 // Copyright (c) 2021 Raphael Champeimont
 
-// Code designed for Arduino Leonardo
+// Code designed for Arduino Uno R3
 
 #include "ArduinoFDC.h"
 
@@ -22,9 +22,7 @@ const byte SECTOR_READ_ORDER[18] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 2, 4, 6, 8, 1
 unsigned int globalSectorCounter = 0;
 
 void setup() {
-  Serial.begin(115200); // used to send debug messages to computer
-  Serial1.begin(1E6, SERIAL_8E1); // used to transmit data to other Arduino
-  //Serial1.begin(9600, SERIAL_8E1); // used to transmit data to other Arduino
+  Serial.begin(1E6, SERIAL_8E1); // used to transmit data to other Arduino
 
   // Start floppy drive
   ArduinoFDC.begin();
@@ -34,7 +32,6 @@ void setup() {
 }
 
 void niceExit() {
-  Serial.println("Exiting");
   ArduinoFDC.motorOff();
   delay(1000);
   exit(0);
@@ -46,7 +43,7 @@ void transmit() {
     // Wait for receiver to be ready to get data
     while (digitalRead(DATA_ASKED_PIN) == LOW);
     // Send batch
-    Serial1.write(buf + 1 + i, SEND_BATCH_SIZE);
+    Serial.write(buf + 1 + i, SEND_BATCH_SIZE);
   }
 }
 
@@ -54,8 +51,6 @@ void loop() {
   byte head, track, sector;
 
   for (track = 0; track < NUM_TRACKS; track++) {
-    Serial.print("Track ");
-    Serial.println(track);
     for (head = 0; head < NUM_HEADS; head++) {
       for (byte i = 0; i < NUM_SECTORS_PER_TRACK; i++) {
         sector = SECTOR_READ_ORDER[i];
@@ -64,10 +59,7 @@ void loop() {
 
         if (status != 0) {
           if (status == S_NOTREADY) {
-            Serial.println("Floppy removed. Restarting.");
             return;
-          } else {
-            Serial.println("Read error");
           }
         } else {
           transmit();
